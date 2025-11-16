a01751387cd5bdca2f32263f8256025ba06ddf0d
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-17 00:58:31 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Barrel exports for all connectors.
+
+export * from "./types/events";
+
+export * from "./gmail/watcher";
+export * from "./gmail/parser";
+
+export * from "./calendar/calendarListener";
+
+export * from "./drive/driveReader";
+
+export * from "./docs/docWriter";
+
+export * from "./meet/transcriptParser";
